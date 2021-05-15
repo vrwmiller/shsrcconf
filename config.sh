3107b977f486e6c64042623b93b252ba5c752e11
Vincent Miller <vrwmiller@gmail.com>
2021-05-15 02:46:13 +0000

    More comments

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-# PENV must be set in the environment
+# PENV must be set in the environment. PENV is plain text password used to
+# encrypt and decrypt the aes-256-cbc encrypted string.
 #
 # Ensure the file below is added to .gitignore and its permissions on disk
 # are 0400. Get a aes-256-cbc encrypted password thusly:
@@ -16,6 +17,7 @@ CONFFILE="config.conf"
 . $CONFFILE
 
 [ $PENV ] && \
+  # decrypt the password
   passwd="`echo $password | openssl base64 -d | \
   openssl enc -d -aes-256-cbc -k $PENV`"
 
