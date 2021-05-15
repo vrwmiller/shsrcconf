d100dcc79d25255c27867ec71a4db56d69b190f3
Vincent Miller <vrwmiller@gmail.com>
2021-05-15 02:41:10 +0000

    enable encrypted passwords

--- config.sh
+++ config.sh
@@ -1,16 +1,24 @@
 #! /bin/sh
 
-# Ensure the following file is added to .gitignore and its permissions on disk
-# are 0400.
+# PENV must be set in the environment
+#
+# Ensure the file below is added to .gitignore and its permissions on disk
+# are 0400. Get a aes-256-cbc encrypted password thusly:
+#
+# `echo 'plaintextpassword' | openssl enc -e -aes-256-cbc -a -k $PENV`
 #
 # Files structure is:
 #
-# username=""
-# password=""
+# username="" # plain text
+# password="" # aes-256-cbc encrypted password
 #
 CONFFILE="config.conf"
-
 . $CONFFILE
 
+[ $PENV ] && \
+  passwd="`echo $password | openssl base64 -d | \
+  openssl enc -d -aes-256-cbc -k $PENV`"
+
 echo "User: $username"
-echo "Pass: $password"
+echo "Encrypted pass: $password"
+echo "Unencrypted pass: $passwd"
